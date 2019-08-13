22b23bcacea0c0977ffe8dd928b466a6fe29d04b
Erlend <eeerlend@gmail.com>
2019-08-13 15:19:38 +0200

    feat: Ported from icloud to variable cloud solution

--- dist/run.sh
+++ dist/run.sh
@@ -2,7 +2,7 @@
 
 #### Package: macup/macup-dotfiles-icloud
 #### Description: macup module that keeps your dotfiles in sync through icloud drive
-function copy_to_icloud {
+function copy_to_cloud {
   local file=$1
   local path_to_dotfiles="$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles
   local path_to_file
@@ -16,11 +16,22 @@ function copy_to_icloud {
   fi
 }
 
+function get_path_to_dotfiles {
+  if macup_dotfiles_cloud_type
+  if [ "$macup_dotfiles_cloud_type" == "dropbox" ]; then
+    echo "$HOME"/Dropbox/.dotfiles
+  elif [ "$macup_dotfiles_cloud_type" == "icloud" ]; then
+    echo "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles
+  else
+    echo "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles
+  fi
+}
+
 function hardlink_dotfile {
   local file=$1
   local chmod=$2
   local num_links=0
-  local path_to_dotfiles="$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles
+  local path_to_dotfiles=$(get_path_to_dotfiles)
   local path_to_file
   path_to_file=$(dirname $file)
 
@@ -40,11 +51,11 @@ function hardlink_dotfile {
     num_links=$(stat -f "%l" "$HOME/$file")
   fi
 
-  # If the file doesn't exist as hard link in your system, we'll create a hard link to icloud
+  # If the file doesn't exist as hard link in your system, we'll create a hard link to cloud
   if [ "$num_links" != "2" ]; then
 
     # shellcheck disable=SC2088
-    report_from_package "~/$file doesn't exist as hard link locally. Will create a hard link to iCloud"
+    report_from_package "~/$file doesn't exist as hard link locally. Will create a hard link to cloud"
 
     if [ -f "$HOME"/"$file" ]; then
       report_from_package "Removing static file $file"
@@ -54,16 +65,16 @@ function hardlink_dotfile {
     if [ -f "$path_to_dotfiles"/"$file" ]; then
       ln "$path_to_dotfiles"/"$file" "$HOME"/"$file"
     else
-      report_from_package "WARN: $file doesn't exist in iCloud. Skipping" 'yellow'
+      report_from_package "WARN: $file doesn't exist in cloud. Skipping" 'yellow'
     fi
   else
     if [ ! -f "$path_to_dotfiles"/"$file" ]; then
-      report_from_package "WARN: $file doesn't exist in iCloud. Removing" 'yellow'
+      report_from_package "WARN: $file doesn't exist in cloud. Removing" 'yellow'
       unlink "$HOME"/"$file"
     fi
 
     # shellcheck disable=SC2088
-    report_from_package "~/$file is already hard linked to iCloud"
+    report_from_package "~/$file is already hard linked to cloud"
   fi
   
   if [ -n "$chmod" ] && [ "$chmod" -eq "$chmod" ] 2>/dev/null; then
@@ -77,31 +88,31 @@ function hardlink_dotfile {
   fi
 }
 
-if [ ! -L .dotfiles-in-icloud ]; then
-  # Check if .dotfiles exist in iCloud drive
-  if [ ! -d "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles ]; then
-    report_from_package "Creating .dotfiles directory in iCloud drive"
-    mkdir "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles
+if [ ! -L .dotfiles-in-cloud ]; then
+  # Check if .dotfiles exist in cloud drive
+  if [ ! -d $(get_path_to_dotfiles) ]; then
+    report_from_package "Creating .dotfiles directory in cloud drive"
+    mkdir $(get_path_to_dotfiles)
   else
-    report_from_package ".dotfiles directory already exist in iCloud drive"
+    report_from_package ".dotfiles directory already exist in cloud drive"
   fi
 
-  report_from_package "Creating a local symlink to the iCloud dotfiles folder (.dotfiles-in-icloud)"
-  ln -s "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles .dotfiles-in-icloud
+  report_from_package "Creating a local symlink to the cloud dotfiles folder (.dotfiles-in-cloud)"
+  ln -s $(get_path_to_dotfiles) .dotfiles-in-cloud
 fi
 
 # todo: check if array is declared up front!
 # shellcheck disable=SC2154
-if [ ${#macup_dotfiles_icloud[@]} -eq 0 ]; then
-  report_from_package "No dotfiles to install. Addd files to the \$macup_dotfiles_icloud array" "yellow"
+if [ ${#macup_dotfiles_cloud[@]} -eq 0 ]; then
+  report_from_package "No dotfiles to install. Addd files to the \$macup_dotfiles_cloud array" "yellow"
 fi
 
-for ((i=0; i<${#macup_dotfiles_icloud[@]}; ++i)); do
-  file="$(echo "${macup_dotfiles_icloud[i]}" | cut -d':' -f1)"
-  chmod="$(echo "${macup_dotfiles_icloud[i]}" | cut -d':' -f2)"
+for ((i=0; i<${#macup_dotfiles_cloud[@]}; ++i)); do
+  file="$(echo "${macup_dotfiles_cloud[i]}" | cut -d':' -f1)"
+  chmod="$(echo "${macup_dotfiles_cloud[i]}" | cut -d':' -f2)"
 
-  if [ ! -f "$HOME"/Library/Mobile\ Documents/com~apple~CloudDocs/.dotfiles/"$file" ]; then
-    copy_to_icloud "$file"
+  if [ ! -f $(get_path_to_dotfiles)/"$file" ]; then
+    copy_to_cloud "$file"
   fi
   
   hardlink_dotfile "$file" "$chmod"
